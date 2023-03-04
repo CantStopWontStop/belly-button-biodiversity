b907b5612c2cbcb6751229897d5683ad7bfb1f76
CantStopWontStop <soried@gmail.com>
2023-03-04 17:42:46 -0500

    Added visualizations

--- static/js/app.js
+++ static/js/app.js
@@ -1,65 +1,169 @@
 const bio_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-const svg = d3.select('svg');
 
-d3.json(bio_data).then(data => {
 
-    // for (let i = 0; i < data.length; i++) {
-    
-    //             // Store the film at index `i` for evaluation
-               
-    //             console.log(data[i])
-    //         }
+// Store in constant the URL of samples.json.
+const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-    for (let i = 0; i < data.samples.length; i++) {
-    
-                    // Store the film at index `i` for evaluation
-                   
-                    console.log(data.samples[i].sample_values)
+// Obtain and store in constant a Promise object from the samples.json URL.
+const promiseObject = d3.json(url);
+
+function run() {
+
+    dropdown();
+
+    promiseObject.then(function(result) {
+        let id = result.names[0];
+
+        demo(id);
+        top10(id);
+        bubblechart(id);
+    });
+};
+
+function dropdown() {
+    promiseObject.then(function(result) {
+        let names = result.names;
+
+        for (let i = 0; i < names.length; i++) {
+            let dropdownOption = d3.select("#selDataset").append("option");
+            dropdownOption.text(names[i]);
+        };
+    });
+};
+
+
+function demo(id) {
+    promiseObject.then(function(result) {
+        let thisID = result.names.findIndex(x => x === id);
+        let metadata = result.metadata[thisID];
+
+        for (let key in metadata) {
+            let demographics_data = d3.select("#sample-metadata").append("p");
+            demographics_data.text(`${key}: ${metadata[key]}`);
+        };
+    });
+};
+
+function top10(id) {
+
+    promiseObject.then(function(result) {
+
+        let thisID = result.names.findIndex(x => x === id);
+        let sample = result.samples[thisID];
+        let otu = sample.otu_ids.slice(0, 10).map(item => `OTU ${item}`);
+        let count = sample.sample_values.slice(0, 10);
+        let labels = sample.otu_labels.slice(0, 10);
+
+        let data = [{
+            type: "bar",
+            x: count,
+            y: otu,
+            text: labels,
+            mode: "markers",
+            marker: {color: '#375a7f'},
+            orientation: "h",
+            transforms: [{
+                type: "sort",
+                target: "x",
+                order: "ascending"
+            }]
+        }];
+
+        let layout = {
+            title: {
+                text:`Top 10 OTUs for Subject ID ${sample.id}`,
+                font:{
+                    color: '#ffffff',
+                    size: 24
                 }
+            } ,  
+            paper_bgcolor: '#303030',
+            plot_bgcolor: '#303030',
+            xaxis: {tickfont: {
+                size: 14,
+                color: '#ffffff'
+              }},
+            yaxis: {
+              titlefont: {
+                size: 16,
+                color: '#ffffff'
+              },
+              tickfont: {
+                size: 14,
+                color: '#ffffff'
+              }
+            }  
+        };
+
+        let config = {responsive: true}
+
+        Plotly.newPlot("bar", data, layout, config);
+    });
+};
+
+function bubblechart (id) {
+    promiseObject.then(function(result) {
+
+        let thisID = result.names.findIndex(x => x === id);
+        let sample = result.samples[thisID];
+        let otu = sample.otu_ids;
+        let count = sample.sample_values;
+        let labels = sample.otu_labels;
+
+        let data = [{
+            x: otu,
+            y: count,
+            text: labels,
+            mode: "markers",
+            marker: {
+                color: count,
+                colorscale: "YlGnBu",
+                size: count,
+                sizeref: 2,
+                sizemin: 4,
+                
+            }
+        }];
+
+        let layout = {
+            title: {
+                text: `Test Subject ID ${sample.id}'s Samples`,
+                font:{color: '#ffffff',
+                size: 24}
+            } , 
+            paper_bgcolor: '#303030',
+            plot_bgcolor: '#303030',
+            xaxis: {tickfont: {
+                size: 14,
+                color: '#ffffff'
+              }},
+            yaxis: {
+              titlefont: {
+                size: 16,
+                color: '#ffffff'
+              },
+              tickfont: {
+                size: 14,
+                color: '#ffffff'
+              }
+            },
+            autosize: true
+        };
+
+        let config = {responsive: true}
+
+        Plotly.newPlot("bubble", data, layout, config);
+    });
+};
+
+function optionChanged(id) {
+    d3.selectAll("p").remove();
+    demo(id);  
     
-    
-    const rects = svg.selectAll('rect')
-        .data(data.samples[0])
-
-        
-    rects.attr('width', d => data.sample_values)
-        .attr('height', 200)
-        .attr('fill', 'orange');
-        
-    rects.enter()
-        .append('rect')
-            .attr('width', d => d.sample_values)
-            .attr('height', 200)
-            .attr('fill', 'orange');
-    
-        
-    //rects.attr;
-    //     // .text(d => d.names)
-    //     // .attr("x", 20)
-    //     // .attr("y", 20);
-
-    // rects.attr('height',200)
-    //     .
-
-});
-// Fetch the JSON data and console log it
-// d3.json(bio_data).then(function(data) {
-//     for (let i = 0; i < data.length; i++) {
-    
-//         // Store the film at index `i` for evaluation
-       
-//         console.log(data[i]);
-//     }
-//   });
+    top10(id);
+    bubblechart(id);
+};
 
 
+run();
 
-
-
-
-
-
-
-    //.attr('width', d => d.)
-    // .attr('height', 320)
-    // .attr('fill', 'black')
